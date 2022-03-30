7ef19e3aad5b7db2caa33e8ca5ae310e7f75b1c2
hmrahim <h.m.rahimnet@gmail.com>
2022-03-31 01:48:16 +0600

    prectice project

---
+++ src/components/Hooks/Hooks.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+const useHooks = () => {
+    const [products, setProducts] = useState([]);
+    useEffect( () =>{
+        fetch('products.json')
+        .then(res=> res.json())
+        .then(data =>setProducts(data))
+    }, []);
+    return [products,setProducts]
+}
+
+export default useHooks
